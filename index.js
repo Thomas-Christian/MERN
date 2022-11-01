ae07a92789665657125d3d4a3cadf826757a7cc6
Brandon Extra <brandex@Brandons-MacBook-Air.local>
2022-11-01 14:36:05 -0400

    heroku cors fix

--- index.js
+++ index.js
@@ -44,6 +44,6 @@ mongoose
   .then(() => console.log("DB connected"))
   .catch((err) => console.error(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
